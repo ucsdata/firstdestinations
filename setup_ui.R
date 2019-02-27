28ef44901c11906f6731f201ff277cbda389c619
Ryan <rdsie@ucsd.edu>
2019-02-27 10:13:31 -0800

    updated packages loaded in setup_ui

--- setup_ui.R
+++ setup_ui.R
@@ -1,5 +1,5 @@
 #load necessary packages 
-packages <- c("leaflet", "plyr", "readxl", "rdrop2", "markdown", "shinythemes", "tidyverse")
+packages <- c("leaflet", "plyr", "readxl", "rdrop2", "markdown", "shinythemes")
 lapply(packages, library, character.only = T)
 
 #store choices for grad years with available data (which folders exist)
